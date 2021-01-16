dba0a26c74022dcf9d940faa92da23200c6d0e6e
Natalia <pr-natal@mail.ru>
2021-01-16 20:36:39 +0300

    file  Program.cs added

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsolApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите своё имя: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Введите дату: ");
+            string date = Console.ReadLine();
+
+            Console.WriteLine("Здраствуйте, {0}, Сегодня {1}. ", name, date);
+        }
+    }
+}
