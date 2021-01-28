743656c3ac8d4a54e925286e3d061a5c550ae5a3
Natalia <pr-natal@mail.ru>
2021-01-28 21:43:09 +0300

    uploade

---
+++ Program12.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            GetFullName("Виолетта", "Стругатская", "Александровна");
+            GetFullName("Максим", "Иванов", "Сергеевич");
+            GetFullName("Мария", "Аризанская", "Вадимовна");
+            GetFullName("Наруто", "Узумаки", "Минатович");
+        }
+
+        static string GetFullName(string firstName, string lastName, string patronymic)
+        {
+            string clued_name = firstName + " " + lastName + " " + patronymic;
+            Console.WriteLine(clued_name);
+           
+            return clued_name;
+        }
+    }
+     
+}
+
