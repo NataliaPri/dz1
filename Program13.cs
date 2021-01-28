db186456ddbca1cefc151b30b555b1def1990d88
Natalia <pr-natal@mail.ru>
2021-01-28 23:26:17 +0300

    uploade new file

---
+++ Program13.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        
+         Console.WriteLine("Введите несколько чисел через пробел:");
+         string s = Console.ReadLine();
+
+            String separator = " ";
+            String[] listOfNumber = s.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+
+            int d = 0;
+            foreach (String a in listOfNumber)
+            {
+                var number = int.Parse(a);
+                d = d + number;
+                
+            }
+         Console.WriteLine("Результат: " + d);
+        }
+    }
+}
