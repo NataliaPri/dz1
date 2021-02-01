b37de356891b8fc20cf80bb2cf2748804bbd6ff5
Natalia <pr-natal@mail.ru>
2021-02-01 22:08:42 +0300

    uploaded new file

---
+++ 5 урок/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            string s = Console.ReadLine();
+            Class1 text = new Class1(s);
+            text.text = "";
+
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ 5 урок/class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp15
+{
+    public class Class1
+    {
+        public string text { get; set; }
+        public Class1(string text)
+        {
+            Console.WriteLine(text);
+        }      
+    }
+}
