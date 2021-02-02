742334bc5f68ece864d3c6dd50c12267e863fe6e
Natalia <pr-natal@mail.ru>
2021-02-02 23:00:02 +0300

    uploaded new file

---
+++ 5 урок/Program16.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string time2 = DateTime.Now.ToString();
+            Console.WriteLine(DateTime.Now);
+            File.AppendAllLines("startup.txt", new[] { time2 });
+            string updateTime = File.ReadAllText("startup.txt");
+            Console.WriteLine(updateTime);
+        }
+    }
+}
