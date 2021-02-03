723d13787379a401590d2921b1f41b9cbbf5f13f
Natalia <pr-natal@mail.ru>
2021-02-03 22:49:33 +0300

    uploaded new file

---
+++ 5 урок/Numbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp17
+{
+    public class Numbers
+    {
+        
+        
+        public string someNum { get; set; }
+       
+
+        public Numbers(string someNum)
+        {
+            String separator = " ";
+            String[] listOfNumber = someNum.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+            
+            foreach (String a in listOfNumber)
+            {
+                int.Parse(a);
+            }
+            Console.WriteLine(listOfNumber);
+
+            return;
+        }
+        
+    }
+}
+
---
+++ 5 урок/Program17.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числа через пробел: ");
+            string s = Console.ReadLine();
+            Numbers text = new Numbers(s);
+            Console.Write(text);
+
+            String separator = " ";
+            String[] listOfNumber = s.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+            
+            byte[] byteNum = new byte[listOfNumber.Length];
+           
+            for (int i = 0; i < listOfNumber.Length; i++)
+            {
+                int x = int.Parse(listOfNumber[i]);
+                byte y = Convert.ToByte(x);
+                byteNum[i] = y;
+            }
+
+            File.WriteAllBytes("bytes.bin", byteNum);
+
+
+        }
+    }
+}
